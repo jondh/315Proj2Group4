f242fab8a22752251e87f881f0d97d7a18459727
Jonathan Harrison <jondh@ymail.com>
2013-06-18 00:30:24 -0500

    Updated Tree
    LET, READ, DATA, and PRINT all partially implemented,
    test contains most of example program

--- Project2/src/com/jondh/project2/ASTtree.java
+++ Project2/src/com/jondh/project2/ASTtree.java
@@ -4,6 +4,14 @@ import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Map;
 
+/* UPDATE: READ, LET, DATA, and PRINT implemented
+ *  Let eval() puts out a dummy value
+ *  I'm not sure about the eval() and print() iteration yet
+ * TODO: put a map with line number and node for looping putposes,
+ * 		 implement errors
+ */
+
+
 //This is the main class that will contain the values for
 //	the data and what the variables are equal to;
 public class ASTtree {
@@ -21,19 +29,21 @@ public class ASTtree {
 		
 		public boolean eval(){
 			if(leftnode == null) return false;
-			while(leftnode.eval()){
+			leftnode.eval();
+			//while(leftnode.eval()){
 				if(rightnode == null) return false;
 				rightnode.eval();
-			}
+			//}
 			return true;
 		}
 		
 		public boolean print(){
 			if(leftnode == null) return false;
-			while(leftnode.print()){
+			leftnode.print();
+			//while(leftnode.print()){
 				if(rightnode == null) return false;
 				rightnode.print();
-			}
+			//}
 			return true;
 		}
 	}
@@ -51,6 +61,10 @@ public class ASTtree {
 				if(data.size()==0) return false;
 				var.put(variables.get(i),data.remove(0));
 			}
+			if(leftnode == null) return false;
+			leftnode.eval();
+			if(rightnode == null) return false;
+			rightnode.print();
 			return true;
 		}
 		
@@ -58,12 +72,15 @@ public class ASTtree {
 			System.out.print(linenumber+" READ ");
 			for(int i = 0; i < variables.size(); i++){
 				System.out.print(variables.get(i));
+				if(i<variables.size()-1) System.out.print(", ");
 			}
+			System.out.print("\n");
 			if(leftnode == null) return false;
-			while(leftnode.print()){
+			leftnode.print();
+			//while(leftnode.print()){
 				if(rightnode == null) return false;
 				rightnode.print();
-			}
+			//}
 			return true;
 		}
 	}
@@ -80,16 +97,100 @@ public class ASTtree {
 		
 		public boolean eval(){
 			var.put(equal, 46.5);
+			if(leftnode == null) return false;
+			leftnode.eval();
+			if(rightnode == null) return false;
+			rightnode.print();
 			return true;
 		}
 		
 		public boolean print(){
 			System.out.println(linenumber+" LET "+equal+" = "+expr);
 			if(leftnode == null) return false;
-			while(leftnode.print()){
+			leftnode.print();
+			//while(leftnode.print()){
 				if(rightnode == null) return false;
 				rightnode.print();
+		//	}
+			return true;
+		}
+	}
+
+	public class ASTdata extends ASTnode{
+		ArrayList<Double> input = new ArrayList<Double>();
+		
+		// The constructor puts the data into the data list
+		//	so that it is avilable from the beginning of the 
+		//	program.
+		ASTdata(ArrayList<Double> dataIn, int lnNum){
+			linenumber = lnNum;
+			input = dataIn;
+			for(int i = 0; i < dataIn.size(); i++){
+				data.add(dataIn.get(i));
 			}
+		}
+		public boolean print(){
+			System.out.print(linenumber+" DATA ");
+			for(int i = 0; i < input.size(); i++){
+				System.out.print(input.get(i));
+				if(i < input.size()-1) System.out.print(", ");
+			}
+			System.out.print("\n");
+			if(leftnode == null) return false;
+			leftnode.print();
+			//while(leftnode.print()){
+				if(rightnode == null) return false;
+				rightnode.print();
+			//}
+			return true;
+		}
+	}
+
+	public class ASTprint extends ASTnode{
+		String expr = "";
+		ArrayList<String> variables = new ArrayList<String>();
+		
+		ASTprint(String statement, int lnNum){
+			expr = statement;
+			linenumber = lnNum;
+		}
+		public ASTprint(ArrayList<String> SvarsIn, int lnNum){
+			variables = SvarsIn;
+			linenumber = lnNum;
+		}
+		public boolean eval(){
+			if(expr == ""){
+				for(int i = 0; i < variables.size(); i++){
+					if(!var.containsKey(variables.get(i))){ return false; } // TODO error: variable not initialized
+					System.out.print(variables.get(i)+" = ");
+					System.out.print(var.get(variables.get(i))+"\n");
+				}
+			}
+			else{
+				System.out.print(expr + "\n");
+			}
+			if(leftnode == null) return false;
+			leftnode.eval();
+			if(rightnode == null) return false;
+			rightnode.print();
+			return true;
+		}
+		public boolean print(){
+			if(expr == ""){
+				System.out.print(linenumber+" PRINT ");
+				for(int i = 0; i < variables.size(); i++){
+					System.out.print(variables.get(i));
+					if(i < variables.size()-1) System.out.print(", ");
+				}
+				System.out.print("\n");
+			}
+			else{
+				System.out.println(linenumber+" PRINT \""+expr+"\"");
+			}
+			if(leftnode == null) return false;
+			leftnode.print();
+			if(rightnode == null) return false;
+			rightnode.print();
 			return true;
 		}
 	}
--- Project2/src/com/jondh/project2/test.java
+++ Project2/src/com/jondh/project2/test.java
@@ -13,17 +13,62 @@ public class test {
 	 * @throws ScriptException 
 	 */
 	public static void main(String[] args) throws ScriptException {
+		// The test program from http://www.cs.bris.ac.uk/~dave/basic.pdf
 		ArrayList<String> vars = new ArrayList<String>();
-		vars.add("D");
+		ArrayList<String> vars30 = new ArrayList<String>();
+		ArrayList<Double> data70 = new ArrayList<Double>();
+		ArrayList<Double> data80 = new ArrayList<Double>();
+		ArrayList<Double> data85 = new ArrayList<Double>();
+		ArrayList<String> pr55 = new ArrayList<String>();
+		vars.add("A1");
+		vars.add("A2");
+		vars.add("A3");
+		vars.add("A4");
+		vars30.add("B1");
+		vars30.add("B2");
+		data70.add(1.0);
+		data70.add(2.0);
+		data70.add(4.0);
+		data80.add(2.0);
+		data80.add(-7.0);
+		data80.add(5.0);
+		data85.add(1.0);
+		data85.add(3.0);
+		data85.add(4.0);
+		data85.add(-7.0);
+		pr55.add("X1");
+		pr55.add("X2");
 		ASTtree tree = new ASTtree();
-		ASTtree.ASTnode node = tree.new ASTnode();
-		tree.root = node;
-		ASTtree.ASTread read = tree.new ASTread(vars, 10);
-		ASTtree.ASTlet let = tree.new ASTlet("D","535+6",20);
-		node.leftnode = read;
-		node.rightnode = let;
-		node.eval();
-		node.print();
+		ASTtree.ASTnode root = tree.new ASTnode();
+		tree.root = root;
+		ASTtree.ASTread read10 = tree.new ASTread(vars, 10);
+		ASTtree.ASTlet let15 = tree.new ASTlet("D","A1*A4-A3*A2",15);
+		// TODO line 20 If D = 0 THEN 65
+		ASTtree.ASTread read30 = tree.new ASTread(vars30,30);
+		ASTtree.ASTlet let37 = tree.new ASTlet("X1","(B1*A4-B2*A2)/D",37);
+		ASTtree.ASTlet let42 = tree.new ASTlet("X2","(A1*B2-A3*B1)/D",42);
+		ASTtree.ASTprint print55 = tree.new ASTprint(pr55,55);
+		// TODO line 60 GO TO 30
+		ASTtree.ASTprint print65 = tree.new ASTprint("NO UNIQUE SOLUTION",65);
+		ASTtree.ASTdata d70 = tree.new ASTdata(data70,70);
+		ASTtree.ASTdata d80 = tree.new ASTdata(data80,80);
+		ASTtree.ASTdata d85 = tree.new ASTdata(data85,85);
+		
+		root.leftnode = read10;
+		read10.leftnode = let15;
+		let15.leftnode = read30;
+		read30.leftnode = let37;
+		let37.leftnode = let42;
+		root.rightnode = print55;
+		print55.leftnode = print65;
+		print65.leftnode = d70;
+		d70.leftnode = d80;
+		d80.leftnode = d85;
+		
+		root.eval();
+		root.print();
+		
+		
 //		ScriptEngineManager mgr = new ScriptEngineManager();
 //	    ScriptEngine engine = mgr.getEngineByName("JavaScript");
 //	    System.out.println(engine.eval("7-5*(8-4)"));
